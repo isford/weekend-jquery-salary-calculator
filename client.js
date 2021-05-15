db179d247f1898489b4a25885516acc950afddc5
Ian <isford7@gmail.com>
2021-05-15 10:21:30 -0500

    Able to add a new row to DOM

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ console.log('JS Loaded');
 
 $(document).ready(readyNow);
 
+let employeeArray = [];
+
 function submitInfo(){
     console.log('Submit button clicked')
 
@@ -15,11 +17,21 @@ const person = {
 }
 
 //APPEND TO TABLE
-    $('#appFn').append(`<td>${person.firstName}</td>`);
-    $('#appLn').append(`<td>${person.lastName}</td>`);
-    $('#appId').append(`<td>${person.id}</td>`);
-    $('#appTitle').append(`<td>${person.title}</td>`);
-    $('#appSalary').append(`<td>${person.annualSalary}</td>`);
+    // $('.table').append(`<tr></tr>`)
+    // $('.table').append(`<td>${person.firstName}</td>`);
+    // $('.table').append(`<td>${person.lastName}</td>`);
+    // $('.table').append(`<td>${person.id}</td>`);
+    // $('.table').append(`<td>${person.title}</td>`);
+    // $('.table').append(`<td>${person.annualSalary}</td>`);
+
+     $('.table').append(`<tr></tr>
+                <td class = "newColumn" id = "appFn">${person.firstName}</td>
+                <td class = "newColumn" id = "appLn">${person.lastName}</td>
+                <td class = "newColumn" id = "appId">${person.id}</td>
+                <td class = "newColumn" id = "appTitle">${person.title}</td>
+                <td class = "newColumn" id = "appSalary">${person.annualSalary}</td>
+                <td></td>
+            </tr>`)
 
     // CLEAR INPUT
     $('#firstIn').val('');
@@ -31,25 +43,28 @@ const person = {
     //TESTER FOR CLICK EVENT
     console.log(person.firstName , person.lastName , person.id , person.title , person.annualSalary )
 
+    employeeArray.push(person);
     
 }
 
-// function newTableRow(){
-//     append(`<tr class = "newTable">
-//                 <td class = "newTable" id = "appFn"></td>
-//                 <td class = "newTable" id = "appLn"></td>
-//                 <td class = "newTable" id = "appId"></td>
-//                 <td class = "newTable" id = "appTitle"></td>
-//                 <td class = "newTable" id = "appSalary"></td>
-//                 <td></td>
-//                 </tr>`)
-// }HOW TO ADD NEW ROW ON BUTTON CLICK?
+function newTableRow(){
+   $('.table').append(`<tr></tr>
+                <td class = "newColumn" id = "appFn">${person.firstName}</td>
+                <td class = "newColumn" id = "appLn">${person.lastName}</td>
+                <td class = "newColumn" id = "appId">${person.id}</td>
+                <td class = "newColumn" id = "appTitle">${person.title}</td>
+                <td class = "newColumn" id = "appSalary">${person.annualSalary}</td>
+                <td></td>
+            </tr>`)
+}
 
 
 function readyNow(){
     console.log('JQ Loaded')
 
-    $('#submit').on('click', submitInfo )
+    $('#submit').on('click', submitInfo)
+
+    // $('.table').on('click', '.newTable', newTableRow)
 
     // $('.parentTable').on('click', '.newTable', newTableRow);  NOT DONE YET
 }
